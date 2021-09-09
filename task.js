2a25384839dd90a542d656e87ddab2408e61ddcb
lkamarova <lilija.kamarova@gmail.com>
2021-09-09 11:05:14 +0400

    Добавила файл css и js

---
+++ task.js
@@ -0,0 +1,13 @@
+var imageSpain = document.getElementById("Spain");
+
+    var makevisas = document.getElementById("visas");
+    window.alert(makevisas.name);
+
+    var tickets = document.getElementById("tickets");
+    window.alert(tickets.name);
+
+    var apartment = document.getElementById("apartment");
+    window.alert(apartment.name);
+
+    var suitcases = document.getElementById("suitcases");
+    window.alert(suitcases.name);
